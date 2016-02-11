ee25ecd4263934cd14ad5aebdad19634858ed358
satoren <satoreyo@hotmail.com>
2016-02-12 05:43:32 +0900

    add luacppinterface implement and result

--- test/luacppinterface.cpp
+++ test/luacppinterface.cpp
@@ -1,4 +1,8 @@
-#include "kaguya/kaguya.hpp"
+
+#include <functional>
+#include <iostream>
+#include <luacppinterface.h>
+
 #include "../benchmark.hpp"
 
 void binding_begin()
@@ -9,38 +13,104 @@ void binding_end()
 }
 const char* binding_name()
 {
-	return "kaguya";
+	return "luacppinterface";
 }
+namespace
+{
+//luacppinterface is bracket operator not supported
+struct GlobalTableWrap
+{
+	LuaTable table_;
+	const char* current_key;
+	GlobalTableWrap(LuaTable& table) :table_(table),current_key(0) {}
+	GlobalTableWrap& operator[](const char* key)
+	{
+		current_key = key;
+		return *this;
+	}
+
+	int operator=(int value)
+	{
+		table_.Set(current_key,value);
+		return value;
+	}
+	operator int()
+	{
+		int v = table_.Get<int>(current_key);
+		current_key = 0;
+		return v;
+	}
+};
+struct LuaTableWrap
+{
+	LuaTable table_;
+	const char* current_key;
+	LuaTableWrap(LuaTable table) :table_(table),current_key(0) {}
+	LuaTableWrap(LuaTable table,const char* key) :table_(table),current_key(key) {}
+	LuaTableWrap operator[](const char* key)
+	{
+		if(current_key)
+		{
+			return LuaTableWrap(table_.Get<LuaTable>(current_key),key);
+		}
+		else
+		{
+			return LuaTableWrap(table_,key);
+		}
+	}
+
+	int operator=(int value)
+	{
+		table_.Set(current_key,value);
+		return value;
+	}
+	operator int()
+	{
+		int v = table_.Get<int>(current_key);
+		current_key = 0;
+		return v;
+	}
+};
+}
+
 void binding_global_table()
 {
-	kaguya::State state;
-	Benchmark::global_table(state);
+	Lua lua;
+	LuaTable global = lua.GetGlobalEnvironment();
+	GlobalTableWrap tablewrap(global);
+	Benchmark::global_table(tablewrap);
 }
+
 
 void binding_table_chain()
 {
-	kaguya::State state;
-	state("t1={t2={t3={}}}");
-	Benchmark::table_chain_access(state);
+	Lua lua;
+	LuaTable global = lua.GetGlobalEnvironment();
+	lua.RunScript("t1={t2={t3={}}}");
+	LuaTableWrap tablewrap(global);
+	Benchmark::table_chain_access(tablewrap);
 }
 
 void binding_native_function_call()
 {
-	kaguya::State state;
-	state["native_function"] = &Benchmark::native_function;
-	state(Benchmark::native_function_lua_code());
+	Lua lua;
+	LuaTable global = lua.GetGlobalEnvironment();
+	auto native_function = lua.CreateFunction<int(int)>(&Benchmark::native_function);
+	
+	global.Set("native_function", native_function);
+	lua.RunScript(Benchmark::native_function_lua_code());
 }
 
 void binding_object_set_get()
 {
-	kaguya::State state;
-	state["SetGet"].setClass(kaguya::ClassMetatable<Benchmark::SetGet>()
-		.addConstructor()
-		.addMember("set", &Benchmark::SetGet::set)
-		.addMember("get", &Benchmark::SetGet::get)
-		);
 
-	Benchmark::SetGet getset;
-	state["getset"] = &getset;
-	state(Benchmark::object_set_get_lua_code());
+Lua lua;
+	LuaTable global = lua.GetGlobalEnvironment();
+auto SetGet = lua.CreateUserdata<Benchmark::SetGet>(new Benchmark::SetGet());
+SetGet.Bind("set", &Benchmark::SetGet::set);
+SetGet.Bind("get", &Benchmark::SetGet::get);
+
+global.Set("getset", SetGet);
+
+	lua.RunScript(Benchmark::object_set_get_lua_code());
 }
