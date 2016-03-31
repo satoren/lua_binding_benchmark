7b61fcf65173cb041032fef960a27cfdb10766cc
Travis CI <satoreyo@hotmail.com>
2016-04-01 05:07:15 +0900

    deploy script miss

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-git init
 git config user.name "Travis CI"
 git config user.email "satoreyo@hotmail.com"
 
-git add . -u
+git add -u
 git commit -m "Deploy to GitHub Pages"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 
