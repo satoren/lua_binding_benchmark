a60348dae56faf56c630b51d6510be56b2c36ecf
Travis CI <satoreyo@hotmail.com>
2016-04-02 03:49:15 +0900

    output timer resolution

--- system_info_dump.linux.sh
+++ system_info_dump.linux.sh
@@ -7,5 +7,10 @@ cat /proc/cpuinfo | grep vendor | uniq
 cat /proc/cpuinfo | grep 'model name' | uniq
 cat /proc/cpuinfo | grep -i mhz | uniq
 echo ""
+echo "----high resolution timer---"
+cat /proc/timer_list | grep resolution | sort | uniq
+echo ""
 echo "----compiler version---"
 c++ --version
+
+
