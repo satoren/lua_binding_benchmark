3fc0e5381a5f5ca6cfb6a5cb854ffc82c1db3750
satoren <satoreyo@hotmail.com>
2016-04-01 23:54:41 +0900

    test for auto deploy

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ git config user.name "Travis CI"
 git config user.email "satoreyo@hotmail.com"
 
 git add -u
+git status
+git branch
 git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 
