a59e5926ff7b760ad41518136cc7e1d7317e214f
Travis CI <satoreyo@hotmail.com>
2016-04-15 04:38:14 +0900

    change output path

--- library_info_dump.lua
+++ library_info_dump.lua
@@ -17,7 +17,7 @@ function split(str, pat)
    return t
 end
 
-out = assert(io.open("library_info.csv","w"))
+out = assert(io.open("result/library_info.csv","w"))
 for submoduleline in io.popen("git submodule status"):lines() do
 local hash,libpath,version = table.unpack(split(submoduleline," "))
 version = string.match(version, '%((.*)%)') or version
