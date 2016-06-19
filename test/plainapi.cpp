b979c4362167fdfafcc4290fd001cce1fca0ad8e
satoren <satoreyo@hotmail.com>
2016-06-19 20:14:49 +0900

    Implement std random bind for plain api

--- test/plainapi.cpp
+++ test/plainapi.cpp
@@ -8,6 +8,41 @@ extern "C" {
 
 BENCHMARK_DEFINE_LIBRARY_NAME("plain api")
 
+inline void stackDump(lua_State *L) {
+	int i;
+	int top = lua_gettop(L);
+	for (i = 1; i <= top; i++) {  /* repeat for each level */
+		int t = lua_type(L, i);
+		switch (t) {
+
+		case LUA_TSTRING:  /* strings */
+			printf("`%s'", lua_tostring(L, i));
+			break;
+
+		case LUA_TBOOLEAN:  /* booleans */
+			printf(lua_toboolean(L, i) ? "true" : "false");
+			break;
+
+		case LUA_TNUMBER:  /* numbers */
+			printf("%g", lua_tonumber(L, i));
+			break;
+		case LUA_TUSERDATA:
+			if (luaL_getmetafield(L, i, "__name") == LUA_TSTRING)
+			{
+				printf("userdata:%s", lua_tostring(L, -1));
+				lua_pop(L, 1);
+				break;
+			}
+		default:  /* other values */
+			printf("%s", lua_typename(L, t));
+			break;
+
+		}
+		printf("  ");  /* put a separator */
+	}
+	printf("\n");  /* end the listing */
+}
+
 struct GlobalTable
 {
 	lua_State *state_;
@@ -281,3 +316,105 @@ RETURN_CLASS_OBJECT_BENCHMARK_FUNCTION_BEGIN
 	lua_close(state);
 }
 RETURN_CLASS_OBJECT_BENCHMARK_FUNCTION_END
+
+
+
+STD_RANDOM_BIND_BENCHMARK_FUNCTION_BEGIN
+{
+#define STD_MT19937_NAME "std.mt19937"
+#define STD_UNI_INT_DIST_NAME "std.uniform_int_distribution"
+
+	using std::mt19937;
+
+	lua_State *L = luaL_newstate(); luaL_openlibs(L);
+	{
+		lua_newtable(L);//create random table
+		int random_table_index = lua_gettop(L);
+
+		//bind mt19937
+		{
+			auto newf = [](lua_State* L) {
+				void* ptr = lua_newuserdata(L, sizeof(mt19937));
+				new(ptr) mt19937(lua_tointeger(L, 1));
+				setmetatable(L, STD_MT19937_NAME);
+				return 1;
+			};
+			auto callf = [](lua_State* L) {
+				mt19937* mtengine = static_cast<mt19937*>(luaL_checkudata(L, 1, STD_MT19937_NAME));
+				lua_pushinteger(L, (*mtengine)());
+				return 1;
+			};
+			auto gcf = [](lua_State* L) {
+				mt19937* mtengine = static_cast<mt19937*>(luaL_checkudata(L, 1, STD_MT19937_NAME));
+				mtengine->~mt19937();
+				return 0;
+			};
+
+			luaL_newmetatable(L, STD_MT19937_NAME);
+			luaL_Reg funcs[] =
+			{
+				{ "new", newf },
+				{ "__gc",gcf },
+				{ "__call",callf },
+				{ 0 ,0 },
+			};
+			setfuncs(L, funcs);
+			lua_newtable(L);
+			luaL_Reg indexfuncs[] =
+			{
+				{ 0 ,0 },
+			};
+			setfuncs(L, indexfuncs);
+			lua_setfield(L, -2, "__index");
+			lua_setfield(L, random_table_index, "mt19937");
+		}
+
+		//bind  std::uniform_int_distribution<int>
+		{
+			typedef std::uniform_int_distribution<int> uni_int_dist;
+
+			auto newf = [](lua_State* L) {
+				void* ptr = lua_newuserdata(L, sizeof(uni_int_dist));
+				new(ptr) uni_int_dist(luaL_optinteger(L, 1,0), luaL_optinteger(L,2, std::numeric_limits<int>::max()));
+				setmetatable(L, STD_UNI_INT_DIST_NAME);
+				return 1;
+			};
+			auto callf = [](lua_State* L) {
+				uni_int_dist* dist = static_cast<uni_int_dist*>(luaL_checkudata(L, 1, STD_UNI_INT_DIST_NAME));
+				mt19937* engine = static_cast<mt19937*>(luaL_checkudata(L, 2, STD_MT19937_NAME));
+				lua_pushinteger(L, (*dist)(*engine));
+				return 1;
+			};
+			auto gcf = [](lua_State* L) {
+				uni_int_dist* dist = static_cast<uni_int_dist*>(luaL_checkudata(L, 1, STD_UNI_INT_DIST_NAME));
+				dist->~uni_int_dist();
+				return 0;
+			};
+
+			luaL_newmetatable(L, STD_UNI_INT_DIST_NAME);
+			luaL_Reg funcs[] =
+			{
+				{ "new", newf },
+				{ "__gc",gcf },
+				{ "__call",callf },
+				{ 0 ,0 },
+			};
+			setfuncs(L, funcs);
+			lua_newtable(L);
+			luaL_Reg indexfuncs[] =
+			{
+				{ 0 ,0 },
+			};
+			setfuncs(L, indexfuncs);
+			lua_setfield(L, -2, "__index");
+			lua_setfield(L, random_table_index, "uniform_int_distribution");
+		}
+		lua_setglobal(L,"random");
+
+		int ret = luaL_dostring(L,lua_code);
+		if (ret != 0) { printf("%s\n", lua_tostring(L, -1)); }
+	}
+	lua_close(L);
+}
+STD_RANDOM_BIND_BENCHMARK_FUNCTION_END
+
