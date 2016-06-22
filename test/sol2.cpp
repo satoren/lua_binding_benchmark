1e928625e1c6dd8d008da644063f0ebe7baf109a
ThePhD <phdofthehouse@gmail.com>
2016-06-22 14:05:10 -0400

    Implement sol2 and sol as promised in #10, which actually exposed a bug in Sol's implementation!

--- test/sol2.cpp
+++ test/sol2.cpp
@@ -62,22 +62,23 @@ RETURN_CLASS_OBJECT_BENCHMARK_FUNCTION_BEGIN
 }
 RETURN_CLASS_OBJECT_BENCHMARK_FUNCTION_END
 
-/* //do not work
 STD_RANDOM_BIND_BENCHMARK_FUNCTION_BEGIN
 {
-	sol::state state;
-	state.open_libraries();
-	state.new_usertype<std::mt19937,int>("random_mt19937",
+	typedef std::uniform_int_distribution<int> uni_int_dist;
+	
+	sol::state lua;
+	lua.open_libraries(sol::lib::base);
+	
+	sol::table random = lua.create_named_table("random");
+	
+	random.new_usertype<std::mt19937,int>("mt19937",
 		"__call", &std::mt19937::operator()
 	);
 
-	typedef std::uniform_int_distribution<int> uni_int_dist;
-	state.new_usertype<uni_int_dist, int,int>("random_uniform_int_distribution",
+	random.new_usertype<uni_int_dist, int,int>("uniform_int_distribution",
 		"__call", [](uni_int_dist& dist, std::mt19937& gen) {return dist(gen); }
 	);
-	//I want register to random.mt19937 ,random.uniform_int_distribution
-	state.script("random={mt19937=random_mt19937,uniform_int_distribution=random_uniform_int_distribution}");
-	state.script(lua_code);
+	
+	lua.script(lua_code);
 }
 STD_RANDOM_BIND_BENCHMARK_FUNCTION_END
-//*/
